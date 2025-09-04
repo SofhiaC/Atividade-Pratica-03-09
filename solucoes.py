7902e1a583ef2a583ccbbabf7fe41a42839abe17
LaraBredow <166045694+LaraBredoww@users.noreply.github.com>
2025-09-03 21:27:26 -0300

    implementação lógica da função de anagrama

--- solucoes.py
+++ solucoes.py
@@ -1,5 +1,19 @@
-def anagrama(palavra1, palavra2):
-    pass
+palavra1 = input("Digite a primeira palavra: ")
+palavra2 = input("Digite a segunda palavra: ")
+
+def sao_anagramas(palavra1, palavra2):
+    if len(palavra1) != len(palavra2):
+        return False
+    
+    verificar1 = sorted(palavra1)
+    verificar2 = sorted(palavra2)
+
+    if verificar1 == verificar2:
+        return True
+    else:
+        return False
 
 def encontrar_maior_palavra(frase):
     pass
+
+print(sao_anagramas(palavra1, palavra2))
